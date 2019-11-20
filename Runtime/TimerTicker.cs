aff932c0b2dad4f6db69a18f5572f6d2315e9ed1
Jeffrey Lanters <jeffrey@hulan.nl>
2019-11-20 17:00:40 +0100

    Create TimerTicker.cs

---
+++ Runtime/TimerTicker.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections;
+using UnityEngine;
+
+namespace UnityPackages { 
+
+  public class TimerTicker : MonoBehaviour {
+    public IEnumerator EnumerateSetTimeout (int miliseconds, Action callback) {
+      yield return new WaitForSeconds (miliseconds / 1000);
+      callback ();
+    }
+
+    public IEnumerator EnumerateSetInterval (int miliseconds, Action callback) {
+      yield return new WaitForSeconds (miliseconds / 1000);
+      callback ();
+      this.StartCoroutine (this.EnumerateSetInterval (miliseconds, callback));
+    }
+  }
+}
