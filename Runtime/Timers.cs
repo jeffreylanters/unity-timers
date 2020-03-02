761573ed6aa81eb3ebb76a375f1530f0a380fd4f
Jeffrey Lanters <jeffrey@hulan.nl>
2020-03-02 14:14:55 +0100

    Made functions available as extension methods

--- Runtime/Timers.cs
+++ Runtime/Timers.cs
@@ -23,5 +23,21 @@ namespace UnityPackages {
     public static void SetInterval (int miliseconds, Action callback) {
       Enumerator.StartCoroutine (Enumerator.EnumerateSetInterval (miliseconds, callback));
     }
+
+    public static void SetTimeout (this Transform target, int miliseconds, Action callback) {
+      Enumerator.StartCoroutine (Enumerator.EnumerateSetTimeout (miliseconds, callback));
+    }
+
+    public static void SetInterval (this Transform target, int miliseconds, Action callback) {
+      Enumerator.StartCoroutine (Enumerator.EnumerateSetInterval (miliseconds, callback));
+    }
+
+    public static void SetTimeout (this GameObject target, int miliseconds, Action callback) {
+      Enumerator.StartCoroutine (Enumerator.EnumerateSetTimeout (miliseconds, callback));
+    }
+
+    public static void SetInterval (this GameObject target, int miliseconds, Action callback) {
+      Enumerator.StartCoroutine (Enumerator.EnumerateSetInterval (miliseconds, callback));
+    }
   }
 }
