96727b5080fca5abf171c9043c8d535a2c8bbd3b
Jeffrey Lanters <jeffreylanters@me.com>
2022-01-07 09:39:47 +0100

    Fixed an issue where miliseconds would not be correctly converted to seconds

--- Runtime/TimerTicker.cs
+++ Runtime/TimerTicker.cs
@@ -5,12 +5,12 @@ using UnityEngine;
 namespace ElRaccoone.Timers {
   public class TimerTicker : MonoBehaviour {
     public IEnumerator EnumerateSetTimeout (int miliseconds, Action callback) {
-      yield return new WaitForSecondsRealtime (miliseconds / 1000);
+      yield return new WaitForSecondsRealtime (miliseconds / 1000f);
       callback ();
     }
 
     public IEnumerator EnumerateSetInterval (int miliseconds, Action callback) {
-      yield return new WaitForSecondsRealtime (miliseconds / 1000);
+      yield return new WaitForSecondsRealtime (miliseconds / 1000f);
       callback ();
       this.StartCoroutine (this.EnumerateSetInterval (miliseconds, callback));
     }
